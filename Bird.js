3a44bfe71cacad1a9195d15f6a74ae469b0179d4
KhalilKray <khalilsblade@gmail.com>
2020-11-17 17:41:12 -0500

    Created Smoke

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory = []
   }
 
   display() {
@@ -9,5 +11,13 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if (this.body.velocity.x > 10 && this.body.position.x > 200) {
+      var position = [this.body.position.x, this.body.position.y]
+      this.trajectory.push(position);
+    }
+    
+    for (var k = 0; k < this.trajectory.length; k++) {
+      image(this.smokeimage, this.trajectory[k][0],this.trajectory[k][1])
+    }
   }
 }
